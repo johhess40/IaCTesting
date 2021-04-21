53f433a3779b85d85db62ec657970bd1804f847c
John Hession <johhess@cdw.com>
2021-04-21 09:08:10 -0700

    Still fixing the stack

--- WebApp.tf
+++ WebApp.tf
@@ -45,10 +45,83 @@ resource "azurerm_app_service" "appServAlpha" {
   app_service_plan_id = azurerm_app_service_plan.appPlanAlpha.id
 
   site_config {
-    dotnet_framework_version = var.dev_app_service.dotnet_framework_version
+    http2_enabled = true
+    always_on = false
+    use_32_bit_worker_process = true
   }
 
   depends_on = [
     azurerm_app_service_plan.appPlanAlpha
   ]
+}
+
+######################################################################
+###Creating a template deployment to designate runtime as .NET CORE###
+######################################################################
+resource "azurerm_template_deployment" "webapp-corestack" {
+  # This will make it .NET CORE for Stack property, and add the dotnet core logging extension
+  name                = "AspNetCoreStack"
+  resource_group_name = azurerm_resource_group.webRG.name
+  template_body       = <<DEPLOY
+{
+    "$schema": "https://schema.management.azure.com/schemas/2019-04-01/deploymentTemplate.json#",
+    "contentVersion": "1.0.0.0",
+    "parameters": {
+        "siteName": {
+            "type": "string",
+            "metadata": {
+                "description": "The Azure App Service Name"
+            }
+        },
+        "extensionName": {
+            "type": "string",
+            "metadata": {
+                "description": "The Site Extension Name."
+            }
+        },
+        "extensionVersion": {
+            "type": "string",
+            "metadata": {
+                "description": "The Extension Version"
+            }
+        }
+    },
+    "resources": [
+        {
+            "apiVersion": "2018-02-01",
+            "name": "[parameters('siteName')]",
+            "type": "Microsoft.Web/sites",
+            "location": "[resourceGroup().location]",
+            "properties": {
+                "name": "[parameters('siteName')]",
+                "siteConfig": {
+                    "appSettings": [],
+                    "metadata": [
+                        {
+                            "name": "CURRENT_STACK",
+                            "value": "dotnetcore"
+                        }
+                    ]
+                }
+            }
+        },
+        {
+            "type": "Microsoft.Web/sites/siteextensions",
+            "name": "[concat(parameters('siteName'), '/', parameters('extensionName'))]",
+            "apiVersion": "2018-11-01",
+            "location": "[resourceGroup().location]",
+            "properties": {
+                "version": "[parameters('extensionVersion')]"
+            }
+        }
+    ]
+}
+  DEPLOY
+  parameters = {
+    "siteName"         = azurerm_app_service.appServAlpha.name
+    "extensionName"    = "Microsoft.AspNetCore.AzureAppServices.SiteExtension"
+    "extensionVersion" = "3.1.7"
+  }
+  deployment_mode = "Incremental"
+  depends_on      = [azurerm_app_service.appServAlpha]
 }
