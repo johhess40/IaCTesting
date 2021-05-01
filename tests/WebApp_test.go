fd1cc8779715de23c32aa475c588b3a3fea05201
John Hession <johhess@cdw.com>
2021-05-01 15:11:04 -0700

    Successful end to end testing with TERRATEST!

--- tests/WebApp_test.go
+++ tests/WebApp_test.go
@@ -6,6 +6,12 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
+var varsFiles = map[string]string{
+	"varsDev":  "webappdev.tfvars",
+	"varsQA":   "webappqa.tfvars",
+	"varsProd": "webappprod.tfvars",
+}
+
 func TestBurlington(t *testing.T) {
 
 	// The path to where our Terraform code is stored
@@ -13,8 +19,31 @@ func TestBurlington(t *testing.T) {
 		TerraformDir: "..",
 	}
 
-	defer terraform.Destroy(t, terraformOptions)
+	// defer terraform.Destroy(t, terraformOptions)
 
-	terraform.InitAndApply(t, terraformOptions)
+	// Running Terraform Init and Plan only and writing it out to a file
 
+	devtfPlan := "webappdev.tfplan"
+
+	qatfPlan := "webappqa.tfplan"
+
+	prodtfPlan := "webappprod.tfplan"
+
+	tfVars := []string{
+		varsFiles["varsDev"],
+		varsFiles["varsQA"],
+		varsFiles["varsProd"],
+	}
+
+	terraform.Init(t, terraformOptions)
+
+	for _, plan := range tfVars {
+		if plan == tfVars[0] {
+			terraform.RunTerraformCommand(t, terraformOptions, terraform.FormatArgs(terraformOptions, "plan", "-var-file="+tfVars[2], "-out="+devtfPlan)...)
+		} else if plan == tfVars[1] {
+			terraform.RunTerraformCommand(t, terraformOptions, terraform.FormatArgs(terraformOptions, "plan", "-var-file="+tfVars[1], "-out="+qatfPlan)...)
+		} else if plan == tfVars[2] {
+			terraform.RunTerraformCommand(t, terraformOptions, terraform.FormatArgs(terraformOptions, "plan", "-var-file="+tfVars[2], "-out="+prodtfPlan)...)
+		}
+	}
 }
